fa19fa61ed5bc5e73ab6d6bc741f9529ea4255a8
Gasper Lukman <gaspercino.lukman@gmail.com>
2024-04-09 17:07:20 +0200

    Fix workflow

--- test_math.py
+++ test_math.py
@@ -1,30 +1,28 @@
 import main
 
-
 def test_add():
-    assert main.add(1,2) == 3
-    assert main.add(3,4) == 7
-    assert main.add(5,6) == 11
+    assert main.add(1, 2) == 3
+    assert main.add(3, 4) == 7
+    assert main.add(5, 6) == 11
 
 
 def test_sub():
     assert main.subtract(3, 1) == 2
-    assert main.subtract(3, -1) == -1
-    assert main.subtract(3, 0) == 0
+    assert main.subtract(3, -1) == 4
+    assert main.subtract(3, 0) == 3
     assert main.subtract(3, 1) == 2
 
 
 def test_mul():
-    assert main.multiply(3, 3) == 6
-    assert main.multiply(3, -1) == -1
+    assert main.multiply(3, 3) == 9
+    assert main.multiply(3, -1) == -3
     assert main.multiply(3, 0) == 0
-    assert main.multiply(3, 1) == 2
+    assert main.multiply(3, 1) == 3
 
 
 def test_div():
     assert main.divide(3, 3) == 1
-    assert main.divide(3, -1) == -1
-    assert main.divide(3, 0) == 0
+    assert main.divide(3, -1) == -3
     assert main.divide(3, 1) == 3
 
 
--- test_string.py
+++ test_string.py
@@ -1,7 +1,7 @@
 import main
 
 
-def testContains():
+def test_Contains():
     text = 'dwaoijng agfjanwgaw g awjg awjg awjg awjg ajg jawg awjg awihgb awuigb aw abc'
     assert main.text_contains_word(text, 'abc') == True
     assert main.text_contains_word(text, '8745') == False
